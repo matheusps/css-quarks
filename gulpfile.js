921baf99accaa3c943ab9bb522b3472db5a19462
matheusps <matheus.ps@icloud.com>
2019-02-22 15:50:41 -0300

    upgrade yarn, better naming

--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,55 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var minifyCss = require('gulp-minify-css');
-var rename = require('gulp-rename');
-var bulkSass = require('gulp-sass-bulk-import');
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var minifyCss = require("gulp-minify-css");
+var rename = require("gulp-rename");
+var bulkSass = require("gulp-sass-bulk-import");
 
 var paths = {
-    sass: ['./src/**/*.scss']
+  sass: ["./src/**/*.scss"]
 };
 
-gulp.task('default', ['build']);
+gulp.task("default", ["build"]);
 
-gulp.task('build', function (done) {
-    gulp.src('./src/css-ninja.scss')
-        .pipe(bulkSass())
-        .pipe(sass())
-        .on('error', sass.logError)
-        .pipe(gulp.dest('./dist'))
-        .pipe(minifyCss({
-            keepSpecialComments: 0
-        }))
-        .pipe(rename({
-            extname: '.min.css'
-        }))
-        .pipe(gulp.dest('./dist'))
-        .on('end', done);
+gulp.task("build", function(done) {
+  gulp
+    .src("./src/css-quarks.scss")
+    .pipe(bulkSass())
+    .pipe(sass())
+    .on("error", sass.logError)
+    .pipe(gulp.dest("./dist"))
+    .pipe(
+      minifyCss({
+        keepSpecialComments: 0
+      })
+    )
+    .pipe(
+      rename({
+        extname: ".min.css"
+      })
+    )
+    .pipe(gulp.dest("./dist"))
+    .on("end", done);
 });
 
-gulp.task('watch', function () {
-    gulp.watch(paths.sass, ['build', 'gen-docs']);
+gulp.task("watch", function() {
+  gulp.watch(paths.sass, ["build", "gen-docs"]);
 });
 
-gulp.task('gen-docs', function() {
-    gulp.src('./src/css-ninja.scss')
-        .pipe(bulkSass())
-        .pipe(sass())
-        .on('error', sass.logError)
-        .pipe(minifyCss({
-            keepSpecialComments: 0
-        }))
-        .pipe(rename({
-            extname: '.min.css'
-        }))
-        .pipe(gulp.dest('./docs'));
+gulp.task("gen-docs", function() {
+  gulp
+    .src("./src/css-quarks.scss")
+    .pipe(bulkSass())
+    .pipe(sass())
+    .on("error", sass.logError)
+    .pipe(
+      minifyCss({
+        keepSpecialComments: 0
+      })
+    )
+    .pipe(
+      rename({
+        extname: ".min.css"
+      })
+    )
+    .pipe(gulp.dest("./docs"));
 });
